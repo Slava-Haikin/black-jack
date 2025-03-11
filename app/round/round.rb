a376a9546d391ed0861b5ee9eba991d36496981e
Slava Haikin <vyachowski@gmail.com>
2025-03-11 23:38:08 +0700

    feat: add round class

---
+++ app/round/round.rb
@@ -0,0 +1,13 @@
+class Round
+  def initialize(interface)
+    @interface = interface
+  end
+
+  def start_first_round
+    puts 'First round started.'
+  end
+
+  def start_new_round
+    puts 'New round started.'
+  end
+end
